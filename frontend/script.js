e4539d8f5a8bdd6eece62a05d806e48e0c5d0d4d
rakhmadi D <rakhmadiwalker@gmail.com>
2020-08-04 11:14:01 +0700

    push

--- frontend/script.js
+++ frontend/script.js
@@ -4,30 +4,44 @@ new Vue({
     el:'#app',
     data(){
         return{
-            datas:[]
+            datas:[],
+            wifi:[]
         }
     },
     mounted() {
-        let n =async()=>{
-            // @ts-ignore
-            let data = await fetch('/show').then(res=>res.json())
-            // @ts-ignore
-            let d =[]
-            let n = []
-            let g=[]
-            for (const key in data) {
-                d.push(key)
-                n.push(data[key])
+        this.rfs()
+        setInterval(()=>{
+            this.rfs()
+            console.log('refs');
+            
+        },2000)
+    },
+    methods: {
+        async rfs(){
+             // @ts-ignore
+             let data = await fetch('/show').then(res=>res.json())
+             let datawifi= await fetch('/show/wifi').then(res=>res.json())
+             // @ts-ignore
+             this.wifi = datawifi
+             // @ts-ignore
+             let d =[]
+             let n = []
+             let g=[]
+             for (const key in data) {
+                 d.push(key)
+                 n.push(data[key])
+             }
+            for (let h = 0; h < d.length; h++) {
+                g.push({
+                    int:d[h],
+                    inf:n[h]
+                })
             }
-           for (let h = 0; h < d.length; h++) {
-               g.push({
-                   int:d[h],
-                   inf:n[h]
-               })
-           }
-           this.datas=g
-           console.log(this.datas);
+            // @ts-ignore
+            this.datas=g
+            // @ts-ignore
+            /////////////////////// 
+
         }
-        n()
     },
 })
--- index.js
+++ index.js
@@ -1,30 +1,50 @@
-//const scanner = require('node-wifi-scanner');
+// @ts-ignore
+// const SerialPort = require('serialport');
+const scanner = require('node-wifi-scanner');
 const Os = require('os');
 const pug = require('pug');
+// @ts-ignore
 const express = require('express')
 const app = express()
+// Promise approach
+// SerialPort.list().then((ports)=>{
+//   ports.forEach((port)=>{
+//     console.log(port)
+    
+//   })
+// })
 app.use(express.static('frontend'))
 const port = 4000
 var NetInter =Os.networkInterfaces()
+let wifiSCan= ''
+ // @ts-ignore
+ scanner.scan((err, networks) => {
+  if (err) {
+    console.error(err);
+    return;
+   }
+   wifiSCan=networks
+ });
+// @ts-ignore
 app.get('/', (req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/html' });
     res.write(pug.renderFile('frontend/index.html'))
     res.end()
 })
+// @ts-ignore
 app.get('/show',(req,res)=>{
         res.writeHead(200, { 'Content-Type': 'application/json' });
         res.write(JSON.stringify(NetInter,null,4))
         res.end()
+})
+// @ts-ignore
+app.get('/show/wifi',(req,res)=>{
+  res.writeHead(200, { 'Content-Type': 'application/json' });
+  res.write(JSON.stringify(wifiSCan,null,4))
+  res.end()
 })
 
 app.listen(port, () => {
   console.log(`http://localhost:${port}`)
 })
 
-// scanner.scan((err, networks) => {
-//   if (err) {
-//     console.error(err);
-//     return;
-//   }
-//   console.log(networks);
-// });
