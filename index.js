9d8672159af28c7a06fa164323da296ba8152fba
rakhmadi D <rakhmadiwalker@gmail.com>
2020-08-05 14:10:29 +0700

    add Interval

--- index.js
+++ index.js
@@ -15,16 +15,25 @@ const app = express()
 // })
 app.use(express.static('frontend'))
 const port = 4000
-var NetInter =Os.networkInterfaces()
+let NetInter =''
 let wifiSCan= ''
  // @ts-ignore
- scanner.scan((err, networks) => {
-  if (err) {
-    console.error(err);
-    return;
-   }
-   wifiSCan=networks
- });
+// @ts-ignore
+let frs=()=>{
+  // @ts-ignore
+  NetInter=Os.networkInterfaces()
+  scanner.scan((err, networks) => {
+    if (err) {
+      console.error(err);
+      return;
+     }
+     wifiSCan=networks
+   });
+}
+setInterval(() => {
+  frs()
+}, 1000);
+// @ts-ignore
 // @ts-ignore
 app.get('/', (req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/html' });
@@ -32,11 +41,13 @@ app.get('/', (req, res) => {
     res.end()
 })
 // @ts-ignore
+// @ts-ignore
 app.get('/show',(req,res)=>{
         res.writeHead(200, { 'Content-Type': 'application/json' });
         res.write(JSON.stringify(NetInter,null,4))
         res.end()
 })
+// @ts-ignore
 // @ts-ignore
 app.get('/show/wifi',(req,res)=>{
   res.writeHead(200, { 'Content-Type': 'application/json' });
